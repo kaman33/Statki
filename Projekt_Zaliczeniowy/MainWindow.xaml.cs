98a55e4520058af37f25e3a352216efbed55d606
erykduc <erykduc@gmail.com>
2026-05-31 01:11:56 +0200

    Szczątkowy zarys mechanik

--- Projekt_Zaliczeniowy/MainWindow.xaml.cs
+++ Projekt_Zaliczeniowy/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Statki_Game;
 
 namespace Projekt_Zaliczeniowy;
 
@@ -16,8 +17,51 @@ namespace Projekt_Zaliczeniowy;
 /// </summary>
 public partial class MainWindow : Window
 {
+
+    private GraStatki _gra = new GraStatki();
     public MainWindow()
     {
         InitializeComponent();
+        UtworzPlansze();
     }
+
+    private void UtworzPlansze()
+    {
+        for (int y = 0; y < 10; y++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                Button przycisk = new Button();
+                przycisk.Margin = new Thickness(2);
+                przycisk.Tag = new Pole(x, y);
+                przycisk.Click += Pole_Click;
+                
+                PlanszaPrzeciwnika.Children.Add(przycisk);
+            }
+        }
+    }
+
+    private void Pole_Click(object sender, RoutedEventArgs e)
+    {
+        Button przycisk = (Button)sender;
+        Pole pole = (Pole)przycisk.Tag;
+        
+        Plansza.WynikStrzalu wynik = _gra.Gracz1Strzela(pole.X, pole.Y);
+        StatusText.Text = wynik.ToString();
+        
+        if (wynik == Plansza.WynikStrzalu.Pudlo)
+        {
+            przycisk.Background = Brushes.LightBlue;
+        }
+        else if (wynik == Plansza.WynikStrzalu.Trafiony)
+        {
+            przycisk.Background = Brushes.Red;
+        }
+        else if (wynik == Plansza.WynikStrzalu.Zatopiony)
+        {
+            przycisk.Background = Brushes.DarkRed;
+        }
+        
+    }
+    
 }
---
+++ Statki_Game/GraStatki.cs
@@ -0,0 +1,40 @@
+﻿namespace Statki_Game;
+
+public class GraStatki
+{
+
+    private Plansza _planszaGracz1;
+    private Plansza _planszaGracz2;
+
+    public GraStatki()
+    {
+        _planszaGracz1 = new Plansza();
+        _planszaGracz2 = new Plansza();
+
+        UstawStatkiTestowe();
+        
+    }
+
+    private void UstawStatkiTestowe()
+    {
+        _planszaGracz1.DodajStatek(new Statek(0, 0, 4, Statek.Orientacja.Pozioma));
+        _planszaGracz1.DodajStatek(new Statek(0, 2, 3, Statek.Orientacja.Pozioma));
+
+        _planszaGracz2.DodajStatek(new Statek(0, 0, 4, Statek.Orientacja.Pozioma));
+        _planszaGracz2.DodajStatek(new Statek(0, 2, 3, Statek.Orientacja.Pozioma));
+        
+    }
+    
+    public Plansza.WynikStrzalu Gracz1Strzela(int x, int y)
+    {
+        return _planszaGracz2.OddajStrzal(x, y);
+    }
+    
+    public Plansza.WynikStrzalu Gracz2Strzela(int x, int y)
+    {
+        return _planszaGracz1.OddajStrzal(x, y);
+    }
+    
+    
+    
+}
